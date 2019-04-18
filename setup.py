ace1b7359fd2415718fd94b013b0bc8f7fc3e36c
Miroslav Zdrale <mzdrale@gmail.com>
2019-04-18 12:08:24 +0200

    Bump version to 0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyubee",
-    version="0.6.dev0",
+    version="0.6",
     author="Miroslav Zdrale",
     author_email="mzdrale@gmail.com",
     description="Simple library for getting stats from Ubee routers.",
